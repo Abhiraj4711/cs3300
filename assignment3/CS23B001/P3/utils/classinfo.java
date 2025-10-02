c3d352c5d1726eeb6bd289309229d1706d691892
Abhiraj4711 <abhirajghumare4711@gmail.com>
2025-10-02 22:54:40 +0530

    this is the assignment 3 commit with everything (i hope) working without handling classes or objects or lambdas

---
+++ assignment3/CS23B001/P3/utils/classinfo.java
@@ -0,0 +1,26 @@
+package utils;
+import java.util.HashMap;
+
+public class classinfo {
+    public String name;
+    public HashMap<String, String> vars=new HashMap<>();
+    public HashMap<String, String> methods=new HashMap<>();
+    public String parent="ThisIsTheFinalClassNoClassAboveThis";
+    // if it is the final class, ie it has no parents its parent will be called : ThisIsTheFinalClassNoClassAboveThis
+    public void addParentVars(allclasses c) {
+        if (!"ThisIsTheFinalClassNoClassAboveThis".equals(parent)) {
+            classinfo ci = c.classes.get(parent);
+            ci.addParentVars(c);
+            ci.vars.forEach((k, v) -> vars.putIfAbsent(k, v));
+
+        }
+    }
+
+    public void addParentMethods(allclasses c){
+        if (!"ThisIsTheFinalClassNoClassAboveThis".equals(parent)) {
+            classinfo ci = c.classes.get(parent);
+            ci.addParentMethods(c);
+            ci.methods.forEach((k, v) -> methods.putIfAbsent(k, v));
+        }
+    }
+}
