0a3239e358f7159273dc35d1b6f913feeec589ca
Abhiraj4711 <abhirajghumare4711@gmail.com>
2025-10-09 20:09:30 +0530

    readding all

---
+++ assignment3/CS23B001/P3/utils/classinfo.java
@@ -0,0 +1,33 @@
+package utils;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+
+public class classinfo {
+    public String name;
+    public HashMap<String, String> vars=new LinkedHashMap<>();
+    public HashMap<String, String> methods=new LinkedHashMap<>();
+    public String parent="ThisIsTheFinalClassNoClassAboveThis";
+    // if it is the final class, ie it has no parents its parent will be called : ThisIsTheFinalClassNoClassAboveThis
+    public void addParentVars(allclasses c) {
+        if (!"ThisIsTheFinalClassNoClassAboveThis".equals(parent)) {
+            classinfo ci = c.classes.get(parent);
+            ci.addParentVars(c);
+            ci.vars.forEach((k, v) -> vars.putIfAbsent(k, v));
+
+        }
+    }
+
+    public void addParentMethods(allclasses c) {
+        if (!"ThisIsTheFinalClassNoClassAboveThis".equals(parent)) {
+            classinfo ci = c.classes.get(parent);
+            ci.addParentMethods(c);
+            
+            ci.methods.forEach((k, v) -> {
+                String methName = k.substring(k.indexOf("___") + 3);
+                if (!methods.containsKey(name + "___" + methName)) {
+                    methods.put(k, v);
+                }
+            });
+        }
+    }
+}
